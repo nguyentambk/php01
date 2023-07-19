b2b77bee49687b3a39a902ed7663d91761c88b63
nguyen tam <nguyentamkhtn108@gmail.com>
2023-07-19 16:09:00 +0700

    connect heroku

--- database_connection.php
+++ database_connection.php
@@ -2,8 +2,12 @@
 
 //database_connection.php
 
+//
 $username = 'root';
-$password = '';
-$connect = new PDO( 'mysql:host=localhost;dbname=reactdb', $username, $password );
+//$password = '';
+//$connect = new PDO( 'mysql:host=localhost;dbname=reactdb', $username, $password );
+$username = 'ba3d95c947c965';
+$password = '0592f928';
+$connect = new PDO( 'mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_4d9a91b6bc4fd52', $username, $password );
 
 ?>
